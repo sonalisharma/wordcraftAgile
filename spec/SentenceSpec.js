58557c7e55e19368bda6da85c27aaaf529a15308
Sonali <sonali.sharma@ischool.berkeley.edu>
2014-04-11 12:59:38 -0700

    Adding js code

---
+++ spec/SentenceSpec.js
@@ -0,0 +1,8 @@
+describe("Hello World Test", function() {
+  it("should return hello world", function() {
+
+  	expect(helloWorld()).toEqual('Hello World');
+    //expect(readFullJson()).toBeGreaterThan(0);
+  });
+
+  });
